bbc20c8e0ea6b722d6a276771cd93c018d3ecb80
yxj0312 <yanxiajun@hotmail.com>
2021-01-31 21:40:56 +0100

    Ep6: Add a route for the new EventDetails component

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import EventList from '../views/EventList.vue'
+import EventDetails from '@/views/EventDetails.vue'
 
 const routes = [
   {
@@ -15,6 +16,11 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/event/123',
+    name: 'EventDetails',
+    component: EventDetails
   }
 ]
 
