e69b9242a1215bffe8500357957b63a7b0d5a0c5
yxj0312 <yanxiajun@hotmail.com>
2021-01-31 21:28:19 +0100

    Ep6: Create a new EventDetails component to display the event’s details

---
+++ src/views/EventDetail.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>{{ event.title }}</h1>
+    <p>{{ event.time }} on {{ event.date }} @ {{ event.location }}</p>
+    <p>{{ event.description }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      event: null
+    }
+  },
+  created() {
+    // fetch event (by id) and set local event data
+  }
+}
+</script>
