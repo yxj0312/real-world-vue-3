1658ee82c763d570f524a1d2d297357f15c42a9f
yxj0312 <yanxiajun@hotmail.com>
2021-03-01 23:18:20 +0100

    Add some const

--- src/views/Capacity.vue
+++ src/views/Capacity.vue
@@ -1,6 +1,12 @@
 <template>
   <div>Capacity: {{ capacity }}</div>
   <button @click="increaseCapacity()">Increase Capacity</button>
+  <h2>Attending</h2>
+  <ul>
+    <li v-for="(name, index) in attending" :key="index">
+      {{ name }}
+    </li>
+  </ul>
 </template>
 
 <script>
@@ -8,12 +14,13 @@ import { ref } from 'vue'
 export default {
   setup() {
     const capacity = ref(3)
+    const attending = ref(['Tim', 'Bob', 'Joe'])
 
     function increaseCapacity() {
       // How we access value on a reactive reference
       capacity.value++
     }
-    return { capacity, increaseCapacity }
+    return { capacity, increaseCapacity, attending }
   }
 }
 </script>
