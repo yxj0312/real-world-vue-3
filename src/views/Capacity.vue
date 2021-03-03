95262b810554d6778c6dc26b1e98bae40f3e3725
yxj0312 <yanxiajun@hotmail.com>
2021-03-03 21:44:56 +0100

    extract a composition function

--- src/views/Capacity.vue
+++ src/views/Capacity.vue
@@ -14,19 +14,24 @@
 import { ref, computed } from 'vue'
 export default {
   setup() {
-    const capacity = ref(3)
-    const attending = ref(['Tim', 'Bob', 'Joe'])
-
-    // eslint-disable-next-line no-unused-vars
-    const spacesLeft = computed(() => {
-      return capacity.value - attending.value.length
-    })
-
-    function increaseCapacity() {
-      // How we access value on a reactive reference
-      capacity.value++
-    }
-    return { capacity, increaseCapacity, attending }
+    return useEventSpace()
   }
+}
+
+// composition function
+function useEventSpace() {
+  const capacity = ref(3)
+  const attending = ref(['Tim', 'Bob', 'Joe'])
+
+  // eslint-disable-next-line no-unused-vars
+  const spacesLeft = computed(() => {
+    return capacity.value - attending.value.length
+  })
+
+  function increaseCapacity() {
+    // How we access value on a reactive reference
+    capacity.value++
+  }
+  return { capacity, increaseCapacity, attending }
 }
 </script>
